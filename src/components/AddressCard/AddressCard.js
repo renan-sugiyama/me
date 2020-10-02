e934e6d3da22bc3ccb4352009f122349fd7c94af
Renan Sugiyama <renan.sugiyama@mblabs.com.br>
2020-10-01 21:54:42 -0300

    node-sass install

---
+++ src/components/AddressCard/AddressCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AddressCard.scss';
+
+export function AddressCard(props) {
+  return (
+    <div className="address-card">
+      AddressCard
+    </div>
+  )
+}
---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AddressCard } from '../components/AddressCard/AddressCard';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <AddressCard />
+    </div>
+  );
+}
+
+export default App;
