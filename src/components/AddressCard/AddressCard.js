f7cae82acc8f83f4aeac6051722699aa31981751
Renan Sugiyama <renan.sugiyama@mblabs.com.br>
2020-10-04 05:09:38 -0300

    fix styles, create header, create supplier card and make responsive

--- src/components/AddressCard/AddressCard.js
+++ src/components/AddressCard/AddressCard.js
@@ -16,16 +16,21 @@ export function AddressCard(
 
   return (
     <div className="address-card d-flex flex-column w-100">
-      <span className="address-card__label">{address.label}</span>
-      <div className="address-card__name">
+
+      <span className="address-card__label mb-2 mb-xl-1">{address.label}</span>
+
+      <div className="address-card__name mb-2 mb-xl-1">
         <span className="address-card__name--title">{address.name}</span>
         {address.code && (<span className="address-card__name--code"> - ({address.code})</span>)}
-
       </div>
-      <span><FiMapPin className="address-card__icon" />{address.address}</span>
-      <span><MdPersonOutline className="address-card__icon" />{address.contact.name}</span>
-      <span><MdMailOutline className="address-card__icon" />{address.contact.email}</span>
-      <div>
+
+      <span className="mb-2 mb-xl-1"><FiMapPin className="address-card__icon" />{address.address}</span>
+
+      <span className="mb-2 mb-xl-1"><MdPersonOutline className="address-card__icon" />{address.contact.name}</span>
+
+      <span className="mb-2 mb-xl-1"><MdMailOutline className="address-card__icon" />{address.contact.email}</span>
+
+      <div className="mb-2 mb-xl-1">
         <span className="mr-3">
           <AiOutlinePhone className="address-card__icon" />{address.contact.phone}
         </span>
@@ -33,6 +38,7 @@ export function AddressCard(
           <FaFax className="address-card__icon" />{address.contact.fax}
         </span>
       </div>
+
     </div >
   )
 }
